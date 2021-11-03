c35f7b666bd345971c4fecff86bd35557a76ff1f
KevBoyz <Kevinho_gameplays@hotmail.com>
2021-11-03 00:26:16 -0300

    project update update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from time import sleep
 import requests as r
 from selenium.webdriver import Chrome
 from selenium.webdriver.common import by
-# from selenium.webdriver import Firefox
+from os import getlogin
 
 
 def bad_status(link, browser):  # Request error case, use selenium
@@ -20,19 +20,31 @@ def bad_status(link, browser):  # Request error case, use selenium
             values.append([title.text, str(price.text).replace('.', '')])
         except:
             pass
-    df = pd.DataFrame(values, columns=['Product Names', 'Prices'])
-    print(df)
+    if values:
+        df = pd.DataFrame(values, columns=['Product Names', 'Prices'])
+        print(f'\n{df}')
+    else:
+        print('Error: Can\'t locate html elements on this page')
+        print('Try a link like this: https://www.amazon.com/s?k=pc')
 
 
 def start(link):
-    site = r.get(link)
-    if site.ok:
-        ...
-    else:
+    try:
+        site = r.get(link)
         if site.status_code == 404:
             print('Not found 404')
         else:
             bad_status(link, Chrome())
+    except Exception as e:
+        print(e)
 
 
-start('https://www.amazon.com/s?k=touchscreen+laptop&crid=2JTPVF06XM1R1&sprefix=touc%2Caps%2C354&ref=nb_sb_ss_ts-doa-p_4_4')
+while True:
+    am_link = str(input('Amazon Search: ')).strip().lower()
+    if am_link.find('www.amazon.com') == -1:
+        print('\nError: Invalid Url, needs point to to amazon.com')
+        print('Try a link like this: https://www.amazon.com/s?k=pc\n')
+    else:
+        break
+start(am_link)
+input(f'\n/{getlogin()}> [Press ENTER to exit] ~ ')
