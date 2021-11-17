bc91490ccf3118b037a72404c8b8d65619cdf17b
KevBoyz <Kevinho_gameplays@hotmail.com>
2021-11-16 21:58:36 -0300

    1.0.0 ver

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from os import getlogin
 from pyautogui import press
 from math import ceil
 import matplotlib.pyplot as plt
-from datetime import datetime
+import numpy as np
 
 
 def amazon_scrap(search, browser):
@@ -20,11 +20,7 @@ def amazon_scrap(search, browser):
     press('enter')  # send the keys
     values = []  # 30-72 items
     sleep(3)
-   # if bool(browser.find_elements(by.By.CSS_SELECTOR, '[class="sg-col-inner"]')):  # Page type: commmon
-        # div_class = '[class="sg-col-inner"]'
-    # else:  # Page type: Hardware
-        # div_class = '[class="a-section"]'
-    for p in range(0, 6):  # Run the first 6 search pages
+    for p in range(0, 6):  # Run the first 6 search pages -- 120-300 elements
         sleep(2)
         divs = browser.find_elements(by.By.CSS_SELECTOR, '[class="sg-col-inner"]')  # Find all items
         for c in range(len(divs)):  # Get data of the item
@@ -40,10 +36,10 @@ def amazon_scrap(search, browser):
     if values:  # All done, return the DataFrame resumed
         df = pd.DataFrame(values, columns=['Product Names', 'Prices'])
         return [ceil(df["Prices"].mean()), len(values), ceil(df["Prices"].sum())]
-    else:     # ^ Prices media - items processed - total value of all items ^
+    else:  # ^ Prices media - items processed - total value of all items ^
         print('\nError: Can\'t locate html elements on this page')
-        print('Check if the page looks like this: https://www.amazon.com/s?k=pc')
-        print('This script don\'t work for some Amazon pages, try search for hardware\n')
+        print('Check if the page looks like this: https://www.amazon.com/s?k=outfit')
+        print('This script don\'t work for some Amazon pages, try search for hardware or common things, like chairs\n')
 
 
 def shopee_scrap(search, browser):
@@ -68,9 +64,11 @@ def shopee_scrap(search, browser):
         try:
             values.append([title.text.strip(), int(str(price.text).replace('R$', ''))])  # Sometimes, return Attribute e
         except:  # Some .text values in titles are like "text". In this case, the text need to be picked manually
-            try:   # Other error can occurred with price if he are bigger than 999.00 he receive 2+ points 1.000.00
+            try:  # Other error can occurred with price if he are bigger than 999.00 he receive 2+ points 1.000.00
                 title = str(title).replace('<div class="_10Wbs- _5SSWfi UjjMrh">', '').replace('</div>', '')
-                price = str(price).replace('<span class="_1d9_77">', '').replace('</span>', '').replace('\"', '').replace(',', '.')
+                price = str(price).replace('<span class="_1d9_77">', '').replace('</span>', '').replace('\"',
+                                                                                                        '').replace(',',
+                                                                                                                    '.')
                 try:
                     values.append([title.strip(), float(price)])  # Prices can return errors
                 except:  # This code fix the None value at price variable
@@ -93,14 +91,12 @@ def graph_output(search, data):
         columns=['Prices media', 'Items analyzed', 'Sum of all'],
         index=['Amazon', 'Shopee']
     )
-    items_analyzed = dt_tb['Items analyzed'].values  # Values iterator
-    prices_media = dt_tb['Prices media'].values  # Values iterator
-    file = open('database.txt', 'r+')  # Saving the data -- on a text file
-    file.write(f'{datetime.now().minute} {prices_media[0]} {prices_media[1]}\n')
-    for line in file.readlines():
-        print(line)
-    file.close()
-    plt.pie(  # Relation of items processed for each site scraped
+    items_analyzed = dt_tb['Items analyzed'].values  # Values iterator    (random ascii that I make; inset/spaceship)
+    prices_media = dt_tb['Prices media'].values  # Values iterator                     ##          \/-kh}---|k
+    file = open('database.txt', 'a')  # Saving the data -- on a text file           --####  -       \/-eo}--|h
+    file.write(f'{search}\n{prices_media[0]} {prices_media[1]}\n')  # #               #  ##          \/-vy}-|b
+    file.close()  # Close                                                           --####  -         \/-bz}|y
+    plt.pie(  # Relation of items processed for each site scraped                      ##              \/\/\|z
         [items_analyzed[0], items_analyzed[1]],  # Values
         labels=['Amazon', 'Shopee'],  # Labels
         autopct="%1.2f%%",  # Show the percentage
@@ -108,7 +104,7 @@ def graph_output(search, data):
     )  # Generating gui graphics - Amazon, Shopee, x?  (consider this on line +- 97)
     plt.title(f'Items processed for site - Search tab: \"{search}\"')
     plt.xlabel(f'Total of items captured: {dt_tb["Items analyzed"].values[0] + dt_tb["Items analyzed"].values[1]}')
-    plt.ylabel(f' Amazon: {items_analyzed[0]}{" "*30}\nShopee: {items_analyzed[1]}{" "*30}').set_rotation(0)
+    plt.ylabel(f' Amazon: {items_analyzed[0]}{" " * 30}\nShopee: {items_analyzed[1]}{" " * 30}').set_rotation(0)
     plt.show()
     plt.bar(  # Prices media graph (bars)
         ['Amazon', 'Shopee'],  # Legends, values
@@ -116,6 +112,33 @@ def graph_output(search, data):
         color=['orange', 'red'])
     plt.title(f'Prices media - {search}')
     plt.xlabel('Scraped sites')
+    plt.show()
+    searches = []
+    amazon_l = []
+    shp_l = []
+    file = open('database.txt', 'r')
+    c = 0
+    for line in file.readlines():
+        c += 1
+        if c % 2 != 0:  # Search
+            searches.append(str(line)[:-1])  # Remove \n
+        else:  # Prices
+            line_iter = line.split()
+            for i in range(len(line_iter)):
+                line_iter[i] = int(line_iter[i])
+                if i % 2 != 0:
+                    amazon_l.append(line_iter[i])
+                else:
+                    shp_l.append(line_iter[i])
+    file.close()
+    plt.figure(facecolor='#cccccc')
+    ax = plt.axes()
+    ax.set_facecolor("#dddddd")
+    plt.plot(searches, amazon_l, color='orange', marker='o')
+    plt.plot(searches, shp_l, color='red', marker='o')
+    plt.title(f'Prices media plot - Last search')
+    plt.legend(['Amazon', 'Shopee'])
+    plt.grid()
     plt.show()
 
 
@@ -136,23 +159,6 @@ def start(search):
 
 
 while True:  # Program interface
-    points = []
-    amazon_l = []
-    shp_l = []
-    file = open('database.txt', 'r')
-    for line in file.readlines():
-        line_read = line.split()
-        for v in range(len(line_read)):  # Converting values to integers
-            if v > 0:
-                line_read[v] = int(line_read[v])
-        points.append(line_read[0])
-        amazon_l.append(line_read[1])
-        shp_l.append(line_read[2])
-    print(amazon_l)
-    print(points)
-    plt.plot(points, amazon_l, marker='o')
-    plt.plot(points, shp_l, marker='o')
-    plt.show()
     search = str(input('\nSearch for: ')).strip()
     if search.find('www.amazon.com') != -1 or search.find('https://www.') != -1:  # Invalid values processing
         print('\nError: This input don\'t accept URL\'s')
